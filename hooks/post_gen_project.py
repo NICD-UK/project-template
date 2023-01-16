e187186a19bcdda9ca7490dd40a75a1fcc282ff1
Matthew Edwards <mt-edwards@protonmail.com>
2023-01-16 13:20:34 +0000

    config.yml is gitignored in post gen project hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,12 @@ import subprocess
 
 python_venv = '{{cookiecutter.python_venv}}'
 
-# add virtual envoronment
+# add virtual environment
 if python_venv == "Yes":   
     subprocess.run(["python3", "-m", "venv", ".venv"])
     subprocess.run([".venv/bin/python", "-m", "pip", "install", "--upgrade", "pip"])
+
+# gitignore config.yml
+with open(".gitignore", "a") as f:
+    lines = ["\n", "# configuration file\n", "config.yml\n"]
+    f.writelines(lines)
