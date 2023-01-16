6db608668ec048fafe6774f675cf84a87e53c0d0
Matthew Edwards <mt-edwards@protonmail.com>
2023-01-16 14:06:16 +0000

    fixed .gitingore issues

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,18 @@
 import subprocess
 
-python_venv = '{{cookiecutter.python_venv}}'
+venv_project = "{{cookiecutter.venv_project}}"
+git_project = "{{cookiecutter.git_project}}"
 
-# add virtual environment
-if python_venv == "Yes":   
-    subprocess.run(["python3", "-m", "venv", ".venv"])
-    subprocess.run([".venv/bin/python", "-m", "pip", "install", "--upgrade", "pip"])
+# createe venv project
+if venv_project == "Yes":   
+    subprocess.run(["python3 -m venv .venv"])
+    subprocess.run([".venv/bin/python -m pip install --upgrade pip"])
+
+# create git project
+if git_project == "Yes":
+    subprocess.run(["git init"])
+    subprocess.run(["git add --all"])
+    subprocess.run(["git commit -m 'initial commit'"])
 
 # gitignore config.yml
 with open(".gitignore", "a") as f:
