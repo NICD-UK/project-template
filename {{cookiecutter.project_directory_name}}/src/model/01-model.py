1e1816c13e4ecfaaef6e4c33e9213fde1083ddd5
Matthew Edwards <mt-edwards@protonmail.com>
2023-03-29 12:01:50 +0100

    added params.yaml and model template script

--- {{cookiecutter.project_directory_name}}/src/model/01-model.py
+++ {{cookiecutter.project_directory_name}}/src/model/01-model.py
@@ -1,10 +1,22 @@
 #%% Load Libraries
 import pandas
 from pathlib import Path
+from sklearn.model_selection import train_test_split
+import yaml
 
 #%% Setup
 root_path = Path(__file__).parent.parent.parent
+with open(root_path / "params.yaml") as f:
+    params = yaml.safe_load(f)["model"]
 data_name = "<data-name>"
 
 #%% Read Data
 wrangle_data = pandas.read_pickle(root_path / f"data/wrangle/{data_name}.pkl")
+
+#%% Split Data
+X_data = wrangle_data.drop(columns=params["target"])
+y_data = wrangle_data[params["target"]]
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data,
+    test_size=params["test_size"], 
+    random_state=params["random_state"],
+)
