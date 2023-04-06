22c8fa0b2d820195ea299b64020e493c90aa5a72
Matthew Edwards <mt-edwards@protonmail.com>
2023-04-06 15:03:00 +0100

    added data splitting

--- {{cookiecutter.project_directory_name}}/src/model/01-model.py
+++ {{cookiecutter.project_directory_name}}/src/model/01-model.py
@@ -7,7 +7,7 @@ import yaml
 #%% Setup
 root_path = Path(__file__).parent.parent.parent
 with open(root_path / "params.yaml") as f:
-    params = yaml.safe_load(f)["model"]
+    params = yaml.safe_load(f)["default"]
 data_name = "<data-name>"
 
 #%% Read Data
@@ -17,6 +17,6 @@ wrangle_data = pandas.read_pickle(root_path / f"data/wrangle/{data_name}.pkl")
 X_data = wrangle_data.drop(columns=params["target"])
 y_data = wrangle_data[params["target"]]
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data,
-    test_size=params["test_size"], 
-    random_state=params["random_state"],
+    train_size=params["split"]["train"], 
+    random_state=params["seed"],
 )
