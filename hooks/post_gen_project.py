4223b424bef400f53d3010f63884989be4d56b08
Matthew Edwards <mt-edwards@protonmail.com>
2024-11-25 09:01:01 +0000

    fixed the new Makefiles and change name of virtual environment

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,6 +12,8 @@ if language == "Python":
     os.rename("Makefile-python", "Makefile")
     os.remove("src/2-clean/README-r.md")
     os.rename("src/2-clean/README-python.md", "src/2-clean/README.md")
+    os.remove("src/3-wrangle/README-r.md")
+    os.rename("src/3-wrangle/README-python.md", "src/3-wrangle/README.md")
     os.remove("{{cookiecutter.project_directory_name}}.Rproj")
     for file in glob.glob("**/*.Rmd", recursive=True):
         os.remove(file)
@@ -22,5 +24,7 @@ if language == "R":
     os.rename("Makefile-r", "Makefile")
     os.remove("src/2-clean/README-python.md")
     os.rename("src/2-clean/README-r.md", "src/2-clean/README.md")
+    os.remove("src/3-wrangle/README-python.md")
+    os.rename("src/3-wrangle/README-r.md", "src/3-wrangle/README.md")
     for file in glob.glob("**/*.py", recursive=True):
         os.remove(file)
